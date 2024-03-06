b1d6875a9119629f864123aa5be2d396b168d86e
pietrocruciata <cruciata9748@gmail.com>
2024-03-06 16:27:15 +0100

    add: fizzbuzz

--- js/app.js
+++ js/app.js
@@ -10,34 +10,22 @@ for (let i = 0; i < 100; i++) {
     // SELEZIONARE I MULTIPLI DI 5
     let numMultiplo5 = parseInt(num % 5); //number
 
-    // STAMPARE "FIZZ" PER I NUMERI MULTIPLI DI 3
+
     if (numMultiplo3 && numMultiplo5 !== 0) {
+
         console.log(num)
+
+        // SELEZIONARE I NUMERI DIVISIBILI SIA PER 3 CHE PER 5
+        // STAMPARE "FIZZBUZZ" PER I NUMERI DIVISIBILI SIA PER 3 CHE PER 5
+    } else if (numMultiplo3 === 0 && numMultiplo5 === 0) {
+        console.log('fizzbuzz')
+
+        // STAMPARE "FIZZ" PER I NUMERI MULTIPLI DI 3
     } else if (numMultiplo3 === 0) {
         console.log('fizz')
 
         // STAMPARE "BUZZ" PER I NUMERI MULTIPLI DI 5
     } else if (numMultiplo5 === 0) {
         console.log('buzz')
-
-        // SELEZIONARE I NUMERI DIVISIBILI SIA PER 3 CHE PER 5
-        // STAMPARE "FIZZBUZZ" PER I NUMERI DIVISIBILI SIA PER 3 CHE PER 5
-    } else if (numMultiplo3 === 0 && numMultiplo5 === 0) {
-        console.log('fizzbuzz')
     }
-
-
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
