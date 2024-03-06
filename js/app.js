827ea9187e4515ba5298df6eb72efdb15e5876a5
pietrocruciata <cruciata9748@gmail.com>
2024-03-06 17:54:32 +0100

    add: DOM and css.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 // STAMPARE IN CONSOLE I NUMERI DA 1 A 100
+
+const scackElement = document.getElementById('scack')
 for (let i = 0; i < 100; i++) {
     let num = i + 1
 
@@ -17,15 +19,24 @@ for (let i = 0; i < 100; i++) {
     // STAMPARE "FIZZBUZZ" PER I NUMERI DIVISIBILI SIA PER 3 CHE PER 5
     if (numMultiplo3 === 0 && numMultiplo5 === 0) {
         console.log('fizzbuzz')
+        scackElement.innerHTML += `<div class= "fizzbuzz">${'fizzbuzz'}</div>`
+
 
         // STAMPARE "FIZZ" PER I NUMERI MULTIPLI DI 3
     } else if (numMultiplo3 === 0) {
         console.log('fizz')
+        scackElement.innerHTML += `<div class= "fizz">${'fizz'}</div>`
 
         // STAMPARE "BUZZ" PER I NUMERI MULTIPLI DI 5
     } else if (numMultiplo5 === 0) {
         console.log('buzz')
+        scackElement.innerHTML += `<div class= "buzz">${'buzz'}</div>`
+
     } else {
         console.log(num)
+        scackElement.innerHTML += `<div class= "num">${num}</div>`
+
     }
 }
+
+
